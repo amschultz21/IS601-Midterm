82521600a614b4d5c18f02cc9d09d20ff09d566d
amschultz21 <196243996+amschultz21@users.noreply.github.com>
2025-03-04 16:39:33 -0500

    modified to get cov'

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,18 +1,12 @@
+import pytest
 from calculator.calculation import Calculation
 
 def test_calculation_operations():
-    calc_add = Calculation(4, 2, "add")
-    assert calc_add.add() == 6
-
-    calc_subtract = Calculation(4, 2, "subtract")
-    assert calc_subtract.subtract() == 2
-
-    calc_multiply = Calculation(4, 2, "multiply")
-    assert calc_multiply.multiply() == 8
-
-    calc_divide = Calculation(4, 2, "divide")
-    assert calc_divide.divide() == 2
+    assert Calculation(4, 2, "add").execute() == 6
+    assert Calculation(4, 2, "subtract").execute() == 2
+    assert Calculation(4, 2, "multiply").execute() == 8
+    assert Calculation(4, 2, "divide").execute() == 2
 
 def test_calculation_divide_by_zero():
-    calc = Calculation(10, 0, "divide")
-    assert calc.divide() is None
+    with pytest.raises(ZeroDivisionError, match="Division by zero is not allowed."):
+        Calculation(10, 0, "divide").execute()
