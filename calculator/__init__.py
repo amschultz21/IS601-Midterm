40959e2bad76871743d4b4f5dfe063fd3db05180
amschultz21 <196243996+amschultz21@users.noreply.github.com>
2025-03-04 16:48:38 -0500

    modified to pass pylint

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -4,18 +4,18 @@ from .operations import add, subtract, multiply, divide
 class Calculator:
     @staticmethod
     def add(a, b):
-        return Calculation(a, b).add()
+        return Calculation(a, b, "add").execute()  #
 
     @staticmethod
     def subtract(a, b):
-        return Calculation(a, b).subtract()
+        return Calculation(a, b, "subtract").execute()
 
     @staticmethod
     def multiply(a, b):
-        return Calculation(a, b).multiply()
+        return Calculation(a, b, "multiply").execute()
 
     @staticmethod
     def divide(a, b):
         if b == 0:
             raise ZeroDivisionError("Division by zero is not allowed.")
-        return Calculation(a, b).divide()
+        return Calculation(a, b, "divide").execute()
